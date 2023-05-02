1f1a855888b32e64927894e2fe7a43eece6e2bce
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2023-05-01 20:32:46 -0400

    commit

---
+++ client/src/App.js
@@ -0,0 +1,83 @@
+import { useContext } from "react";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import {
+  createBrowserRouter,
+  Navigate,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import LeftBar from "./components/leftbar/LeftBar";
+import NavBar from "./components/navbar/NavBar";
+import RightBar from "./components/rightbar/RightBar";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+import "./style.scss"
+import { darkModeContext } from "./context/darkModeContext";
+import { AuthContext } from "./context/authContext";
+
+function App() {
+  const {currentUser} = useContext(AuthContext);
+
+  const {darkMode} = useContext(darkModeContext)
+
+  const Layout = () => {
+    return (
+      <div className={`theme-${darkMode ? "dark" : "light"}`}>
+        <NavBar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <div style={{flex: 6}}>
+            <Outlet />
+          </div>
+          <RightBar />
+        </div>
+      </div>
+    );
+  };
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
